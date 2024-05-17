4a38f8a6e4db96c59c862b9e1172b92394d5e661
Nikhil S <nikhilsnayak3473@gmail.com>
2024-05-17 18:41:55 +0530

    style: fix email and bot icon

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -4,8 +4,8 @@ import { NavLink } from './nav-link';
 
 export function Header() {
   return (
-    <header className='sticky top-0 z-50 w-full backdrop-blur'>
-      <div className='mx-auto flex max-w-screen-lg items-center gap-4 p-2'>
+    <header className='sticky top-0 z-50 w-full shadow-md backdrop-blur'>
+      <div className='mx-auto flex max-w-screen-lg items-center gap-4 px-2 py-4'>
         <nav className='flex flex-grow items-center justify-between'>
           <ul className='flex gap-2'>
             <li>
