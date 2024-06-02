2bbfe2524bcab1e44e86d7a762c3fabd757bce8b
Nikhil S <nikhilsnayak3473@gmail.com>
2024-06-02 12:18:22 +0530

    feat: imporve og image api

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -6,17 +6,37 @@ export function GET(request: Request) {
 
   return new ImageResponse(
     (
-      <div tw='flex flex-col w-full h-full items-center justify-center bg-white'>
-        <div tw='flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8'>
-          <h2 tw='flex flex-col text-4xl font-bold tracking-tight text-left'>
-            {title}
-          </h2>
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          justifyContent: 'center',
+          background: 'linear-gradient(to right, #000000, #434343)',
+        }}
+      >
+        <div
+          style={{
+            marginLeft: 190,
+            marginRight: 190,
+            display: 'flex',
+            fontSize: 130,
+            letterSpacing: '-0.05em',
+            fontStyle: 'normal',
+            color: 'white',
+            lineHeight: '120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          {title}
         </div>
       </div>
     ),
     {
-      width: 1200,
-      height: 630,
+      width: 1920,
+      height: 1080,
     }
   );
 }
