eb693252a2043df98a6c3976f75246bc8f87e8fd
Nikhil S <nikhilsnayak3473@gmail.com>
2024-06-25 21:50:17 +0530

    style: animate bubble

--- app/blogs/[slug]/funko-pop-zoro.tsx
+++ app/blogs/[slug]/funko-pop-zoro.tsx
@@ -1,6 +1,11 @@
 'use client';
 import { FunkoPopZoroSvg } from '@/assets/icons';
-import { motion, useMotionValueEvent, useScroll } from 'framer-motion';
+import {
+  AnimatePresence,
+  motion,
+  useMotionValueEvent,
+  useScroll,
+} from 'framer-motion';
 import Link from 'next/link';
 import { useState } from 'react';
 
@@ -33,26 +38,39 @@ export function FunkoPopZoro({ blogTitle }: FunkoPopZoroProps) {
     >
       <div className='relative'>
         <FunkoPopZoroSvg />
-        {isVisible && (
-          <div className='absolute -top-32 right-16 max-w-sm rounded-lg border border-gray-300 bg-white p-4 shadow-lg'>
-            <p className='text-sm text-gray-700'>
-              Hey there! Do you know where I can find my swords? Just kidding. I
-              can summarize this blog for you{' '}
-              <Link
-                href={{
-                  pathname: '/bot',
-                  query: {
-                    ...(blogTitle && { prompt: `Summarize ${blogTitle} blog` }),
-                  },
-                }}
-                className='text-blue-500 underline'
-              >
-                click here
-              </Link>
-            </p>
-            <div className='absolute -bottom-2 right-8 h-0 w-0 border-l-[10px] border-r-[10px] border-t-[10px] border-l-transparent border-r-transparent border-t-white'></div>
-          </div>
-        )}
+        <AnimatePresence>
+          {isVisible && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{
+                duration: 1,
+              }}
+              //@ts-expect-error -- I don't know why this error is coming
+              className='absolute -top-32 right-16 max-w-sm rounded-lg border border-gray-300 bg-white p-4 shadow-lg'
+            >
+              <p className='text-sm text-gray-700'>
+                Hey there! Do you know where I can find my swords? Just kidding.
+                I can summarize this blog for you{' '}
+                <Link
+                  href={{
+                    pathname: '/bot',
+                    query: {
+                      ...(blogTitle && {
+                        prompt: `Summarize ${blogTitle} blog`,
+                      }),
+                    },
+                  }}
+                  className='text-blue-500 underline'
+                >
+                  click here
+                </Link>
+              </p>
+              <div className='absolute -bottom-2 right-8 h-0 w-0 border-l-[10px] border-r-[10px] border-t-[10px] border-l-transparent border-r-transparent border-t-white'></div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </motion.div>
   );
