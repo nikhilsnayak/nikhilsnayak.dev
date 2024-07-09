65e99d96b80f6bc6ab4fbe437048db99a981189e
Nikhil S <nikhilsnayak3473@gmail.com>
2024-07-09 19:53:57 +0530

    feat: add other activites section in work page

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,3 +1,3 @@
 export { default as profile } from './profile.jpg';
-export { default as gImage1 } from './gallery-1.jpg';
+export { default as techMang24 } from './tech-mang-24.jpg';
 export { default as zoro } from './zoro.png';
