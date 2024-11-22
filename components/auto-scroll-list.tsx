4de8c4ec1a5d2587f9e0a149164fc816009f2289
Nikhil S <nikhilsnayak3473@gmail.com>
2024-11-22 11:58:22 +0530

    fix: auto scroll list for touch devices (#292)

--- components/auto-scroll-list.tsx
+++ components/auto-scroll-list.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps, RefCallback, useCallback, useState } from 'react';
+import { ComponentProps } from 'react';
 
 import { cn } from '~/lib/utils';
 
@@ -6,53 +6,91 @@ export function AutoScrollList({
   children,
   className,
   ...rest
-}: Omit<ComponentProps<'ul'>, 'ref' | 'onWheel'>) {
-  const [shouldAutoScroll, setShouldAutoScroll] = useState(true);
-
-  const scrollAreaRef: RefCallback<HTMLUListElement> = useCallback(
-    (list) => {
-      if (!list) return;
-      const listEnd = list.querySelector('li[data-list-end]');
-
-      const observer = new MutationObserver(() => {
-        if (shouldAutoScroll) {
-          listEnd?.scrollIntoView({ behavior: 'smooth', block: 'end' });
-        }
-      });
-
-      observer.observe(list, {
-        childList: true,
-        subtree: true,
-      });
-
-      return () => {
-        observer.disconnect();
-      };
-    },
-    [shouldAutoScroll]
-  );
-
+}: Omit<ComponentProps<'ul'>, 'ref'>) {
   return (
     <ul
       ref={scrollAreaRef}
       className={cn('overflow-y-auto', className)}
-      onWheel={(e) => {
-        const { scrollHeight, clientHeight, scrollTop } = e.currentTarget;
-        const maxScrollHeight = scrollHeight - clientHeight;
-
-        if (e.deltaY < 0) {
-          setShouldAutoScroll(false);
-        } else if (
-          e.deltaY > 0 &&
-          maxScrollHeight - scrollTop <= maxScrollHeight / 2
-        ) {
-          setShouldAutoScroll(true);
-        }
-      }}
       {...rest}
     >
       {children}
       <li data-list-end />
     </ul>
   );
+}
+
+function scrollAreaRef(list: HTMLUListElement) {
+  if (!list) return;
+
+  let shouldAutoScroll = true;
+  let touchStartY = 0;
+  let lastScrollTop = 0;
+
+  const checkScrollPosition = () => {
+    const { scrollHeight, clientHeight, scrollTop } = list;
+    const maxScrollHeight = scrollHeight - clientHeight;
+    const scrollThreshold = maxScrollHeight / 2;
+
+    if (scrollTop < lastScrollTop) {
+      shouldAutoScroll = false;
+    } else if (maxScrollHeight - scrollTop <= scrollThreshold) {
+      shouldAutoScroll = true;
+    }
+
+    lastScrollTop = scrollTop;
+  };
+
+  const handleWheel = (e: WheelEvent) => {
+    if (e.deltaY < 0) {
+      shouldAutoScroll = false;
+    } else {
+      checkScrollPosition();
+    }
+  };
+
+  const handleTouchStart = (e: TouchEvent) => {
+    touchStartY = e.touches[0].clientY;
+  };
+
+  const handleTouchMove = (e: TouchEvent) => {
+    const touchEndY = e.touches[0].clientY;
+    const deltaY = touchStartY - touchEndY;
+
+    if (deltaY < 0) {
+      shouldAutoScroll = false;
+    } else {
+      checkScrollPosition();
+    }
+
+    touchStartY = touchEndY;
+  };
+
+  const handleScroll = () => {
+    checkScrollPosition();
+  };
+
+  list.addEventListener('wheel', handleWheel, { passive: true });
+  list.addEventListener('touchstart', handleTouchStart, { passive: true });
+  list.addEventListener('touchmove', handleTouchMove, { passive: true });
+  list.addEventListener('scroll', handleScroll, { passive: true });
+
+  const listEnd = list.querySelector('li[data-list-end]');
+  const observer = new MutationObserver(() => {
+    if (shouldAutoScroll) {
+      listEnd?.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    }
+  });
+
+  observer.observe(list, {
+    childList: true,
+    subtree: true,
+  });
+
+  return () => {
+    observer.disconnect();
+    list.removeEventListener('wheel', handleWheel);
+    list.removeEventListener('touchstart', handleTouchStart);
+    list.removeEventListener('touchmove', handleTouchMove);
+    list.removeEventListener('scroll', handleScroll);
+  };
 }
