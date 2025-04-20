6079e50f2a09fcd9d38d05a33d32d3bcf74b25d4
Nikhil S <nikhilsnayak3473@gmail.com>
2025-04-20 07:42:18 +0000

    fix

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -51,7 +51,7 @@ export async function generateMetadata({
       type: 'article',
       siteName: 'Nikhil S - Blog',
       publishedTime: publishedAt.toDateString(),
-      url: `${BASE_URL}/blogs/${slug}`,
+      url: `${BASE_URL}/blog/${slug}`,
       images: [
         {
           url: ogImage,
