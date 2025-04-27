8b8c5558ece11c2b6a89e675cdaf78d0f4e204a9
Nikhil S <nikhilsnayak3473@gmail.com>
2025-04-27 22:09:08 +0530

    radix ui (#431)
    * radix ui
    
    * update deps

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import * as React from 'react';
-import * as DialogPrimitive from '@radix-ui/react-dialog';
 import { XIcon } from 'lucide-react';
+import { Dialog as DialogPrimitive } from 'radix-ui';
 
-import { cn } from '~/lib/utils/index';
+import { cn } from '~/lib/utils';
 
 function Dialog({
   ...props
