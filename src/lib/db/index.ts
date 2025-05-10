587c831a786aadad735cdaad3c71ef912aaac7f2
Nikhil S <nikhilsnayak3473@gmail.com>
2025-05-10 17:57:09 +0530

    migrate db sdk from @vercel/postgres to @neondatabase/serverless (#446)
    * migrate db sdk from @vercel/postgres to @neondatabase/serverless
    
    * update deps

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,6 +1,8 @@
-import { sql } from '@vercel/postgres';
-import { drizzle } from 'drizzle-orm/vercel-postgres';
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 
 import * as schema from './schema';
 
-export const db = drizzle(sql, { schema });
+const client = neon(process.env.DATABASE_URL!);
+
+export const db = drizzle({ client, schema });
