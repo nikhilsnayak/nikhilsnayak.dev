867521cb58c77bad82e4bc89e9e986281bf892d3
Nikhil S <nikhilsnayak3473@gmail.com>
2025-06-25 05:21:48 +0530

    Update footer to include viewport once

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -167,6 +167,7 @@ export default function RootLayout({
               </main>
               <Remount on='path-change'>
                 <motion.footer
+                  viewport={{ once: true }}
                   initial={{ opacity: 0, y: 20 }}
                   whileInView={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.4, delay: 0.8 }}
